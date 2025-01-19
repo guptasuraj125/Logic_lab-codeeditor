ec7308cb04615b1139620f4497d55b6cfe172123
Suraj <surajgupta05086@gmail.com>
2025-01-19 15:47:32 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+export default {
+  providers: [
+    {
+      domain: "https://star-sole-58.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+
+//https://star-sole-58.clerk.accounts.dev by jwt template
